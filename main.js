56b11aa4088995014555d8e2f2fdce33355e294e
Yash Srivastava <80906833+yash7526@users.noreply.github.com>
2022-12-10 15:07:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
-const abc = require("fs");
-abc.readFile("./books/AliceInWonderland.txt", "utf-8", file);
+(Array.from(document.getElementsByTagName("a"))).forEach((a) => {
+    console.log("assd");
+    a.addEventListener("click", book);
+});
 
-function file(e, data) {
-    console.log(data);
+
+
+function book(e) {
+    e.preventDefault();
+    console.log(e.target.id);
+
+    if (e.target.id == "c") {
+        read("./books/LOTR.txt", "Lord Of The Rings");
+    }
+
+
+    if (e.target.id == "a") {
+        read("./books/AliceInWonderland.txt", "Alice In WonderLand");
+    }
+
+    if (e.target.id == "b") {
+        read("./books/JekyllAndHyde.txt", "Jekyll And Hyde");
+    }
+
+}
+
+
+function read(url, title) {
+
+    var c = new XMLHttpRequest();
+    c.open('GET', url);
+    c.onreadystatechange = function() {
+        var t = c.responseText;
+        document.getElementById("title").innerText = title;
+        document.getElementById("book").innerText = t;
+    }
+    c.send();
 }
